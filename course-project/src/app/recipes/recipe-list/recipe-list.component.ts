02d6d3102aee98992adb37ab9cc0fc5a6d9a5fb2
Jordi Raya Lapuente <raya.jordi@gmail.com>
2018-05-23 15:57:44 +0200

    Section 3: Course Project - The basics

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a test recipe',
+      'https://www.savorytooth.com/wp-content/uploads/2016/01/chicken-tikka-masala-bowl-square.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is another test recipe',
+      'https://www.savorytooth.com/wp-content/uploads/2016/01/chicken-tikka-masala-bowl-square.jpg'
+    )
+  ];
 
   constructor() { }
 
---
+++ course-project/src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient {
+
+  // the "public" accessor creates a property -> this.name = name, etc.
+  constructor(public name: string, public amount: number) {
+  }
+}
